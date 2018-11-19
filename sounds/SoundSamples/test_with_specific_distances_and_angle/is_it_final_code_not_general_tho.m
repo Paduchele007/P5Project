893f3e2d769d7929aaae90345adf5531dc82f01b
whoelse <tlasic16@student.aau.dk>
2018-11-19 13:53:02 +0100

    recordings 80 and 90 deg
    veri gud

--- sounds/SoundSamples/test_with_specific_distances_and_angle/is_it_final_code_not_general_tho.m
+++ sounds/SoundSamples/test_with_specific_distances_and_angle/is_it_final_code_not_general_tho.m
@@ -3,6 +3,6 @@ clear;
 load('RecordSession15thNov.mat')
 l  = length(dataRB)
 delay=6
-is_it_final_code_probs_not(delay, dataRB, dataLB,l);
+[newB,noiseB,shiftedLB]=is_it_final_code_probs_not(delay, dataRB, dataLB,l);
 
  
