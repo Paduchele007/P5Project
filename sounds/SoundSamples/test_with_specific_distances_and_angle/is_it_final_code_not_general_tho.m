450a982d26026555640b3fe0dde55fd4b722dfd9
whoelse <tlasic16@student.aau.dk>
2018-12-05 20:36:12 +0100

    Matlab code now matches what we explain
    its not so important, but it does its own thing with one person

--- sounds/SoundSamples/test_with_specific_distances_and_angle/is_it_final_code_not_general_tho.m
+++ sounds/SoundSamples/test_with_specific_distances_and_angle/is_it_final_code_not_general_tho.m
@@ -2,7 +2,8 @@ clc;
 clear;
 load('already_shifted_vectors.mat')
 l=length(dataLL)
-delay=8
-[new,noise,shiftedL]=is_it_final_code_probs_not(delay, dataLL, dataRL,l);
-
- 
+delay=9
+[new,noise,shiftedL]=is_it_final_code_probs_not(delay, dataRL, dataLL,l);
+audiowrite('mufi.flac',new,48000);
+audiowrite('noiseMufi.flac',noise,48000);
+audiowrite('mufiShifted.flac',shiftedL,48000);
--- sounds/SoundSamples/test_with_specific_distances_and_angle/is_it_final_code_probs_not.m
+++ sounds/SoundSamples/test_with_specific_distances_and_angle/is_it_final_code_probs_not.m
@@ -1,13 +1,13 @@
 function [new,noise,shifted] = is_it_final_code_probs_not(delay, Right, Left,l)
 %% delay so the phase matches
 
-for i=1:l-delay
-    shifted(i)=Left(i+delay);
+for i=1+delay:l
+    shifted(i)=Left(i-delay);
 end
 shifted=transpose(shifted);
 %% substract signals from each other = get noise
 for i=1:l-delay
-    noise(i)=shifted(i)-Left(i);
+    noise(i)=shifted(i)-Right(i);
 end
  noise=transpose(noise);
 %% substract noise from original = filtered
@@ -17,4 +17,5 @@ end
 
 new=transpose(new);
 end
+
 
